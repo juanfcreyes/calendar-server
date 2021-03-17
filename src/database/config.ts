f39e4dd452c069800cae96f483da25784da57469
juanfcreyes <juanfcreyes@hotmail.com>
2021-03-17 10:40:56 -0500

    log url database

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,7 +3,8 @@ import mongoose  from "mongoose";
 export const dbConnection = async () => {
     try {
         mongoose.connect(process.env.DB_CNN, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
-        console.error('DB Online');
+        console.log(process.env.DB_CNN);
+        console.log('DB Online');
     } catch (error) {
         console.error(error);
         throw new Error("Error initialization db");
